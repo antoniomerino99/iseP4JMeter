addb0be023f3e7df0a82acb5350364877d0a251a
AntonioMerino <antoniomerino99@gmail.com>
2020-06-11 18:50:12 +0200

    Cambio a protocolo https en el fichero de prueba.

--- pruebaEntorno.sh
+++ pruebaEntorno.sh
@@ -4,7 +4,7 @@ TOKEN=$(curl -s \
 -u etsiiApi:laApiDeLaETSIIDaLache \
 -d "login=mariweiss@tropoli.com&password=anim" \
 -H "Content-Type: application/x-www-form-urlencoded" \
--X POST http://$SERVER/api/v1/auth/login)
+-X POST https://$SERVER/api/v1/auth/login)
 
 resultado=$?
 if test "$resultado" != "0"; then
@@ -13,4 +13,4 @@ fi
 
 curl \
 -H "Authorization: Bearer $TOKEN" \
-http://$SERVER/api/v1/alumnos/alumno/mariweiss%40tropoli.com
+https://$SERVER/api/v1/alumnos/alumno/mariweiss%40tropoli.com
